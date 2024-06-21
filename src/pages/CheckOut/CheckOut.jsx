288bd466a41a7370450425b90741233f53f535a8
miyaggiemiami <clementmorare971@gmail.com>
2024-06-22 01:08:04 +0200

    .

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import add from '../../assets/Increase-Button.svg'
+import { Link } from 'react-router-dom'
+import plus from '../../assets/Plus-Button.svg'
+import minus from '../../assets/Minus-Button.svg'
 
-const CheckOut = () => {
-  return (
-    <div>
+function CheckOut() {
+    const [productsState, setProductsState] = useState(
+      products.map(product => ({ ...product, quantity: 1 }))
+    );
+  
+    const updateQuantity = (productId, increment) => {
+      setProductsState(prevProducts => {
+        const newProducts = [...prevProducts];
+        const productToUpdate = newProducts.find(product => product.id === productId);
+        if (productToUpdate) {
+          productToUpdate.quantity += increment ? 1 : -1;
+        }
+        return newProducts;
+      });
+    };
 
-    </div>
-  )
-}
-
-export default CheckOut
+export default CheckOut;
