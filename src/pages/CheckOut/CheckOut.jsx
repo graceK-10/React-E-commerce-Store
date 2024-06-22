698c1cf908db2f69ead5952d09236f2ad4f51803
miyaggiemiami <clementmorare971@gmail.com>
2024-06-22 17:50:51 +0200

    fixed a function

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -4,19 +4,21 @@ import plus from '../../assets/Plus-Button.svg'
 import minus from '../../assets/Minus-Button.svg'
 
 function CheckOut() {
-    const [productsState, setProductsState] = useState(
-      products.map(product => ({ ...product, quantity: 1 }))
-    );
+  const [productsState, setProductsState] = useState(
+    products.map(product => ({ ...product, quantity: 1 }))
+  );
+
+  const updateQuantity = (productId, increment) => {
+    setProductsState(prevProducts => {
+      const newProducts = [...prevProducts];
+      const productToUpdate = newProducts.find(product => product.id === productId);
+      if (productToUpdate) {
+        productToUpdate.quantity += increment ? 1 : -1;
+      }
+      return newProducts;
+    });
+  };
+}
   
-    const updateQuantity = (productId, increment) => {
-      setProductsState(prevProducts => {
-        const newProducts = [...prevProducts];
-        const productToUpdate = newProducts.find(product => product.id === productId);
-        if (productToUpdate) {
-          productToUpdate.quantity += increment ? 1 : -1;
-        }
-        return newProducts;
-      });
-    };
 
 export default CheckOut;
