c6c1a6780fa86534868af05cd70ce5a77c72599b
miyaggiemiami <clementmorare971@gmail.com>
2024-06-23 15:44:20 +0200

    styled container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductDetails from "./pages/ProductDetails";
+import CheckOut from "./pages/CheckOut/CheckOut"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/item/:id" element={<ProductDetails />} />
+        <Route path="/Checkout" element={<CheckOut />} />
       </Routes>
     </Router>
   );
