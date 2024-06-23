ebbaeb59500858fc88c4582a9299320df08ecf42
KIMBERLY_TAPFUMANEYI_KIMBERLY <kimberlytapfumanyi16@gmail.com>
2024-06-23 23:30:15 +0200

    final output

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductDetails from "./pages/ProductDetails";
 import Bag from "./components/Bag";
 import CheckOut from "./pages/CheckOut/CheckOut";
 import ShippingAddressForm from "./components/ShippingAddressForm";
+import AddPayment from "./Components/AddPayment";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/item/:id" element={<ProductDetails />} />
         <Route path="/Bag" element={<Bag />} />
         <Route path="/Checkout" element={<CheckOut />} />
+        <Route path="/AddPayment" element={<AddPayment />} />
         <Route path="/ShippingAddressForm" element={<ShippingAddressForm />} />
       </Routes>
     </Router>
